1beaf4d9b3635192274111d85e8520d09dcd394c
Ömer Özen, PhD Candidate <omerozen1@gmail.com>
2024-07-07 22:02:46 +0300

    version check test

--- pages/api/appversion.js
+++ pages/api/appversion.js
@@ -4,7 +4,7 @@ import validateApiKey from '../../middleware/validateApiKey';
 export default function handler(req, res) {
   validateApiKey(req, res, () => {
     if (req.method === 'GET') {
-      res.status(200).json({ version: '1.0.0' }); // Replace with the latest app version
+      res.status(200).json({ version: '1.0.1' }); // Replace with the latest app version
     } else {
       res.setHeader('Allow', ['GET']);
       res.status(405).end(`Method ${req.method} Not Allowed`);
