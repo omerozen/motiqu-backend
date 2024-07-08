d6b1c1468239c28b69b7fe284d023835b6d398fd
Ömer Özen, PhD Candidate <omerozen1@gmail.com>
2024-07-08 20:24:55 +0300

    terms - privacy api calls secured

--- pages/api/terms.js
+++ pages/api/terms.js
@@ -1,14 +1,17 @@
-import Cors from 'cors';
+// pages/api/terms.js
 import initMiddleware from '../../lib/init-middleware';
+import { apiKeyMiddleware } from '../../lib/api-key-middleware';
 import terms from '../../data/terms.json';
 
-const cors = initMiddleware(
-  Cors({
-    methods: ['GET'],
-  })
-);
+const apiKeyValidator = initMiddleware(apiKeyMiddleware);
 
 export default async function handler(req, res) {
-  await cors(req, res);
-  res.status(200).json(terms);
+  await apiKeyValidator(req, res);
+
+  if (req.method === 'GET') {
+    res.status(200).json(terms);
+  } else {
+    res.setHeader('Allow', ['GET']);
+    res.status(405).end(`Method ${req.method} Not Allowed`);
+  }
 }
