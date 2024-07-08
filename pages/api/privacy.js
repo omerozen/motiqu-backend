84899cac9b6cf3f3961962874f035b9df24034f8
Ömer Özen, PhD Candidate <omerozen1@gmail.com>
2024-07-08 18:56:32 +0300

    terms version fixed

--- pages/api/privacy.js
+++ pages/api/privacy.js
@@ -2,9 +2,9 @@ import { promises as fs } from 'fs';
 import path from 'path';
 
 export default async function handler(req, res) {
-  const filePath = path.join(process.cwd(), 'data', 'terms.json');
+  const filePath = path.join(process.cwd(), 'data', 'privacy.json');
   const fileContents = await fs.readFile(filePath, 'utf8');
-  const terms = JSON.parse(fileContents);
+  const privacy = JSON.parse(fileContents);
 
-  res.status(200).json(terms);
+  res.status(200).json(privacy);
 }
