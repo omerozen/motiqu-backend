932b55aa3e594622c4d62d51a87c2bf04f15c5d6
Ömer Özen, PhD Candidate <omerozen1@gmail.com>
2024-10-06 14:40:52 +0300

    lang fetch system changed fixed6

--- pages/api/translations/[lang].js
+++ pages/api/translations/[lang].js
@@ -6,21 +6,24 @@ import fs from 'fs';
 
 export default async function handler(req, res) {
   try {
-    // Validate the API key using the custom middleware
+    // Validate the API key
     validateApiKey(req, res, () => {});
 
     // Get the language code from the query
     const { lang } = req.query;
 
-    // Resolve file path based on language
+    // Construct file path
     const filePath = path.join(process.cwd(), 'data', `${lang}.json`);
+    console.log("File Path being used:", filePath);  // Log the file path for debugging
 
     // Read the file and return the contents if it exists
     if (fs.existsSync(filePath)) {
+      console.log(`Language file found for: ${lang}`);  // Log success message
       const fileContents = fs.readFileSync(filePath, 'utf8');
       res.setHeader('Content-Type', 'application/json');
       return res.status(200).send(fileContents);
     } else {
+      console.log(`Language file not found for: ${lang}`);  // Log error message if file not found
       return res.status(404).json({ error: 'Language file not found' });
     }
   } catch (error) {
